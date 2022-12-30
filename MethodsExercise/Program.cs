5a5d7d650f77cd9431cbce03c272a325ca4f02b9
Doug Finney <dugfiny@gmail.com>
2022-12-29 19:51:23 -0600

    Completed MethodExercise

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -1,10 +1,42 @@
-﻿namespace MethodsExercise
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace MethodsExercise
 {
     public class Program
     {
+        public static void BeastBathSoap()
+        {
+            Console.WriteLine("What is your name?");
+            string userName = Console.ReadLine();
+
+            Console.WriteLine($"Hi, {userName}. What's your favorite color?");
+            string faveColor = Console.ReadLine();
+
+            Console.WriteLine($"What's your favorite animal, {userName}?");
+            string faveAnimal = Console.ReadLine();
+
+            Console.WriteLine($"What's your favorite scent, {userName}?");
+            string faveScent = Console.ReadLine();
+
+            string beastBathSoap = $"Thank you, {userName}." +
+                $" we are shipping your order of {faveAnimal} shaped, {faveColor} colored and {faveScent} scented Beast Body Soap today!";
+            Console.WriteLine(beastBathSoap);
+        }
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+
+            BeastBathSoap();
+
+
+
+
+
         }
+
+
+
     }
 }
